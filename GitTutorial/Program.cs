56341433e3eaf71a58d94f9905094d3610089a7e
Clement Azibataram <clementazibataram@gmail.com>
2022-09-28 21:18:36 +0100

    displayed hello git

--- GitTutorial/Program.cs
+++ GitTutorial/Program.cs
@@ -7,6 +7,7 @@ namespace GitTutorial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello git");
         }
     }
 }
